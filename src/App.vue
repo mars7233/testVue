9c19df8047c2de6ba3e795f66ce958b34c13f17d
mars <mars2022@vip.qq.com>
2018-03-07 17:06:22 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
-  <div id="app">
+  <div>
     <img src="./assets/logo.png">
-    <router-view/>
+    {{mars}}
+    <ul>
+      <li v-for="(item,index) in list">{{item.name+"-"+item.price}}</li>
+    </ul>
+    <input type="text">
+    <button v-on:click="addItem">addItem</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      mars: "hehehehehehehe",
+      list: [
+        {
+          name: "apple",
+          price: 10
+        },
+        {
+          name: "peal",
+          price: 10
+        }
+      ]
+    };
+  },
+  methods: {
+    addItem() {
+      console.log(this.list);  
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
